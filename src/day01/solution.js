34f44c97242415f7a8cabf31ed50b33509d01896
Francis <hisconer@gmail.com>
2020-12-08 12:33:28 +0100

    Update day01 part 2

--- src/day01/solution.js
+++ src/day01/solution.js
@@ -1,12 +1,20 @@
 const { readFileSync } = require("fs")
-const { arrayMult, getTwoMagicNumbers } = require("./index")
+const {
+  arrayMult,
+  getTwoMagicNumbers,
+  getThreeMagicNumbers,
+} = require("./index")
 
 const inputFile = `./src/day01/input`
 const rawInput = readFileSync(`${inputFile}`, "utf8")
 const arrayInput = rawInput.split("\n").filter(x => x !== "")
 
 // Part 1
-console.log(arrayMult(getTwoMagicNumbers(arrayInput, 2020)))
+console.log(
+  `Day 01. Part 1:\n-> ${arrayMult(getTwoMagicNumbers(arrayInput, 2020))}`
+)
 
 // Part 2
-//console.log(arrayMult(getTwoMagicNumbers(arrayInput, 2020)))
+console.log(
+  `Day 01. Part 2:\n-> ${arrayMult(getThreeMagicNumbers(arrayInput, 2020))}`
+)
