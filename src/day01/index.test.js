e8bc80ca44a461578a38d9e3da3fdec1da9ed1bf
Francis <hisconer@gmail.com>
2020-12-08 10:28:57 +0100

    Add day01 start

---
+++ src/day01/index.test.js
@@ -0,0 +1,14 @@
+const { sum } = require("./index")
+
+describe("Day 1: Report Repair", () => {
+  describe("Part 1", () => {
+    test("should...", () => {
+      expect(sum(1, 2)).toBe(3)
+    })
+  })
+  describe("Part 2", () => {
+    test("should...", () => {
+      expect(sum(1, 2)).toBe(3)
+    })
+  })
+})
